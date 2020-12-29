49a2bff4362ab98058f836fa91f35149ce0cbcb3
robmarcotte <marcotte.rt@gmail.com>
2020-12-29 17:26:30 -0500

    added trained random forest models, updated soj_g function

---
+++ R/soj_g.R
@@ -0,0 +1,109 @@
+# Soj-g function from Marcotte et al 2021
+#
+# Function to apply the Soj-g method to raw wrist accelerometer data
+#
+# Library dependencies: matrixStats, data.table, zoo, dplyr, randomForest
+
+soj_g = function(filepath, export_format = 'session', freq = 80, step1_sd_threshold = .00375, step2_nest_length = 5, step3_nest_length = 60, step3_orig_soj_length_min = 180){
+
+  # Assuming that the filepath provided is an .rds file
+  data = readRDS(filepath)
+
+  # Step 1 - Identify likely inactive periods
+  data_summary = data.frame(index = 1:(nrow(data)/freq), sd_vm = rowSds(matrix(data$VM, ncol = freq, byrow = T)))
+  data_summary$step1_estimate = ifelse(data_summary$sd_vm <=step1_sd_threshold, 1, 0) # 1 = inactive, 0 = unclassified
+
+  seconds_index = seq(1, nrow(data), by = freq)
+  diffs = which((dplyr::lag(data_summary$step1_estimate) != data_summary$step1_estimate) == T)
+  diffs = c(1, diffs)
+  data_summary$step2_sojourn_index = NA
+  data_summary$step2_sojourn_duration = NA
+
+  # Step 2 - Segment remaining unlabeled periods into smaller windows, identify whether inactive or active
+  data_summary$step2_sojourn_index = data.table::rleid(data_summary$step1_estimate)
+  data_summary$step2_sojourn_duration[diffs] = rle(data_summary$step2_sojourn_index)[[1]]
+  data_summary$step2_sojourn_duration = zoo::na.locf(data_summary$step2_sojourn_duration)
+  data_summary$step2_sojourn_index = sort(unlist(tapply(data_summary$step2_sojourn_index, data_summary$step2_sojourn_index, nest_sojourn, nest_length = step2_nest_length)))
+
+  data$step1_sd_vm = rep(data_summary$sd_vm, each = freq)
+  data$step1_estimate = rep(data_summary$step1_estimate, each = freq)
+  data$step2_sojourn_index = rep(data_summary$step2_sojourn_index, each = freq)
+  data$step2_sojourn_duration = rep(data_summary$step2_sojourn_duration, each = freq)
+
+  ag_step2_summary = ag_feature_calc(data, samp_freq = 80, window = 'sojourns', soj_colname = 'step2_sojourn_index', seconds_colname = 'step2_sojourn_duration')
+  ag_step2_summary$step2_durations = rle(data_summary$step2_sojourn_index)[[1]]
+
+  ag_step2_summary$step2_estimate = predict(PAMethods::stage2_unclassified_rf, newdata = ag_step2_summary, type = 'class')
+
+  data_summary$step2_estimate = rep(ag_step2_summary$step2_estimate, times = ag_step2_summary$step2_durations)
+  data_summary$step2_estimate = ifelse(data_summary$step1_estimate == 1, 1, data_summary$step2_estimate)
+  data_summary$step3_sojourn_index = NA
+  data_summary$step3_sojourn_duration = NA
+  diffs = which((dplyr::lag(data_summary$step2_estimate) != data_summary$step2_estimate) == T)
+  diffs = c(1, diffs)
+
+  data_summary$step2_estimate = factor(data_summary$step2_estimate, levels = c(1,2), labels =c('Stationary','Active'))
+  data_summary$step3_sojourn_index = data.table::rleid(data_summary$step2_estimate)
+  data_summary$step3_sojourn_duration[diffs] = rle(data_summary$step3_sojourn_index)[[1]]
+  data_summary$step3_sojourn_duration =zoo::na.locf(data_summary$step3_sojourn_duration)
+  data_summary$step3_sojourn_index = sort(unlist(tapply(data_summary$step3_sojourn_index, data_summary$step3_sojourn_index, nest_sojourn2, orig_soj_length_min = step3_orig_soj_length_min, nest_length = step3_nest_length)))
+
+  data$step2_estimate = rep(data_summary$step2_estimate, each = freq)
+  data$step3_sojourn_index = rep(data_summary$step3_sojourn_index, each = freq)
+  data$step3_sojourn_duration = rep(data_summary$step3_sojourn_duration, each = freq)
+
+  # Compute features in final sojourns
+  ag_step3_summary = ag_feature_calc(data, samp_freq = 80, window = 'sojourns', soj_colname = 'step3_sojourn_index', seconds_colname = 'step3_sojourn_duration')
+  ag_step3_summary$step3_durations = rle(data_summary$step3_sojourn_index)[[1]]
+  final_step2_estimate = data_summary %>% group_by(step3_sojourn_index) %>%
+    summarize(step2_estimate = dplyr::first(step2_estimate)) %>% select(step2_estimate) %>% ungroup() %>% as.vector()
+  ag_step3_summary$step2_estimate = final_step2_estimate$step2_estimate
+
+  ag_step3_summary$step3_estimate_intensity = predict(PAMethods::stage3_intensity_rf, newdata = ag_step3_summary, type = 'class')
+  ag_step3_summary$step3_estimate_type = predict(PAMethods::stage3_activity_rf, newdata = ag_step3_summary, type = 'class')
+  ag_step3_summary$step3_estimate_locomotion = predict(PAMethods::stage3_locomotion_rf, newdata = ag_step3_summary, type = 'class')
+
+  if(export_format == 'session'){
+    session_summary = data.frame(Filename = basename(filepath),
+                                 Sedentary_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_intensity == 'Sedentary')]),
+                                 Light_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_intensity == 'Light')]),
+                                 Moderate_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_intensity == 'Moderate')]),
+                                 Vigorous_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_intensity == 'Vigorous')]),
+                                 MVPA_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_intensity == 'Moderate' | ag_step3_summary$step3_estimate_intensity == 'Vigorous')]),
+                                 Sitting_Lying_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_type == 'Sitting_Lying')]),
+                                 Stationary_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_type == 'Stationary+')]),
+                                 Walking_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_type == 'Walking')]),
+                                 Running_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_type == 'Running')]),
+                                 Locomotion_minutes = sum(ag_step3_summary$step3_durations[which(ag_step3_summary$step3_estimate_locomotion == 'Locomotion')]),
+                                 Total_minutes = sum(ag_step3_summary$step3_durations),
+                                 stringsAsFactors = F)
+    session_summary[,2:ncol(session_summary)] = session_summary[,2:ncol(session_summary)]/60
+
+    return(session_summary)
+  }
+
+  if(export_format == 'sojourn'){
+    return(ag_step3_summary)
+  }
+
+  if(export_format == 'seconds'){
+    data_summary$step3_estimate_intensity = rep(ag_step3_summary$step3_estimate_intensity, times = ag_step3_summary$step3_durations)
+    data_summary$step3_estimate_type = rep(ag_step3_summary$step3_estimate_type, times = ag_step3_summary$step3_durations)
+    data_summary$step3_estimate_locomotion = rep(ag_step3_summary$step3_estimate_locomotion, times = ag_step3_summary$step3_durations)
+
+    return(data_summary)
+  }
+
+  if(export_format == 'raw'){
+    data_summary$step3_estimate_intensity = rep(ag_step3_summary$step3_estimate_intensity, times = ag_step3_summary$step3_durations)
+    data_summary$step3_estimate_type = rep(ag_step3_summary$step3_estimate_type, times = ag_step3_summary$step3_durations)
+    data_summary$step3_estimate_locomotion = rep(ag_step3_summary$step3_estimate_locomotion, times = ag_step3_summary$step3_durations)
+
+    data$step3_estimate_intensity = rep(data_summary$step3_estimate_intensity, each = freq)
+    data$step3_estimate_type = rep(data_summary$step3_estimate_type, each = freq)
+    data$step3_estimate_locomotion = rep(data_summary$step3_estimate_locomotion, each = freq)
+
+    return(data)
+  }
+
+}
